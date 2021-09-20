90a44ad02dbca8da211e34496d679c2d0039bd93
Michael Seitz <michael.s.seitz@gmail.com>
2021-09-20 17:03:19 +0200

    inner functions working now

--- calculator.js
+++ calculator.js
@@ -1,13 +1,42 @@
 function sum(a, b) {
-  return;
+  if (a === undefined && b === undefined)
+    return 0
+  
+  if (a === undefined)
+    return 0+b
+  else if (b === undefined)
+    return a+0
+
+  let result
+  result = a+b
+  return result;
 }
 
 function subtract(a, b) {
+  if (a === undefined && b === undefined)
+  return 0
+
+  if (a === undefined)
+    return 0-b
+  else if (b === undefined)
+    return a-0
+
+let result
+result = a-b
+return result;
+
   return;
 }
 
 function divide(a, b) {
-  return;
+  let result
+
+  if (b !== 0) {
+    result = a/b
+    return result
+  }
+  else
+    throw "Error"
 }
 
 function multiply(a, b) {
